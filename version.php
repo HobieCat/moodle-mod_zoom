ddd391f3ca6081f047fe968ed0fbebd35c5e6b4a
Giorgio Consorti <g.consorti@lynxlab.com>
2024-01-03 18:27:18 +0100

    bump version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_zoom';
-$plugin->version = 2024010300;
+$plugin->version = 2024010301;
 $plugin->release = 'v5.1.4';
 $plugin->requires = 2019052000;
 $plugin->maturity = MATURITY_STABLE;
